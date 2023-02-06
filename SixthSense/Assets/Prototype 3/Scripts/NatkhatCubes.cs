b1b56b8e990128ecef164361d6fd53ac13461f6b
modanivn <103077198+modanivn@users.noreply.github.com>
2023-02-05 22:18:52 -0800

    Fixing debug

--- SixthSense/Assets/Prototype 3/Scripts/NatkhatCubes.cs
+++ SixthSense/Assets/Prototype 3/Scripts/NatkhatCubes.cs
@@ -30,7 +30,6 @@ public class NatkhatCubes : MonoBehaviour
             jumpForceDecrease();
             break;
         }
-        Debug.Log(index);
     }
 
     public void bridgeRotateIncreaseSpeed(){
