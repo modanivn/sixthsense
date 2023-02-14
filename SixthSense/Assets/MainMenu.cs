47446386683507707b7a69ac8163b141a8d32143
Rishikesh Jaadhav <rishikeshjaadhav@gmail.com>
2023-02-13 18:44:56 -0800

    Main Menu Added

--- SixthSense/Assets/MainMenu.cs
+++ SixthSense/Assets/MainMenu.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
     public void PlayGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1f;
     }
     public void QuitGame() {
         Debug.Log("Quit");
