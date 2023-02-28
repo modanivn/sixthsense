96715988c654e32321a10f303c03a7999076b182
Rishikesh Jaadhav <rishikeshjaadhav@gmail.com>
2023-02-27 21:52:19 -0800

    debug commit

--- SixthSense/Assets/Level 1/Scripts/FeedMonsterTrigger_Level1.cs
+++ SixthSense/Assets/Level 1/Scripts/FeedMonsterTrigger_Level1.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class FeedMonsterTrigger_Level1 : MonoBehaviour
 {
+    public CanvasGroup nextScenePanel;
+    // public CanvasGroup gameOverPanel;
     public GameObject player;
     private void OnTriggerEnter(Collider other){
         if(other.tag == "Player"){
             player.GetComponent<StackingPrototype3_Level1>().checkEndCondition();
+            nextScenePanel.alpha = 1f;
         }
+    }
+
+    public void nextScene() {
+        SceneManager.LoadScene(2);
+        nextScenePanel.alpha = 0f;
     }
 }
--- SixthSense/Assets/Level3/Scripts/FeedMonsterTrigger_Level3.cs
+++ SixthSense/Assets/Level3/Scripts/FeedMonsterTrigger_Level3.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FeedMonsterTrigger_Level3 : MonoBehaviour
 {
+    public CanvasGroup nextScenePanel;
     public GameObject player;
     private void OnTriggerEnter(Collider other){
         if(other.tag == "Player"){
             player.GetComponent<StackingPrototype3_Level3>().checkEndCondition();
+            nextScenePanel.alpha = 1f;
         }
+    }
+     public void nextScene() {
+        SceneManager.LoadScene(4);
+        nextScenePanel.alpha = 0f;
     }
 }
