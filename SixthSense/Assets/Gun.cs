db88d9890a6b07dd91e7182958f38dc0c22226f8
Rishikesh Jaadhav <rishikeshjaadhav@gmail.com>
2023-03-02 20:11:14 -0800

    drop shadow added
    Few testing changes

---
+++ SixthSense/Assets/Gun.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10;
+
+    void Update() {
+        // if(Input.GetKeyDown(KeyCode.Space))
+    }
+}
