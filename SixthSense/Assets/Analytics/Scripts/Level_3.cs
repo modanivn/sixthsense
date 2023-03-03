dd513b8dc0edeb3c9148835f85ff66742d1f47b4
himanshu-ashar <h.h.ashar@gmail.com>
2023-03-02 18:34:33 -0800

    level 3 analytic structure

---
+++ SixthSense/Assets/Analytics/Scripts/Level_3.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Level_3
+{
+    // Start is called before the first frame update
+    public int totalNumberOfPowerups;
+    public int totalNumberOfFreeze;
+    public int totalNumberOfHits;
+    public int totalNumberOfFalls;
+    public float timeToComplete;
+    public bool isComplete;
+    public string hitLocations;
+    //public int cubesCollected;
+    public Level_3(int totalNumberOfFreeze, int totalNumberOfHits, int totalNumberOfFalls, float timeToComplete, bool isComplete, string hitLocations)
+    {
+        this.totalNumberOfFreeze = totalNumberOfFreeze;
+        this.totalNumberOfHits = totalNumberOfHits;
+        this.totalNumberOfFalls = totalNumberOfFalls;
+        this.timeToComplete = timeToComplete;
+        this.totalNumberOfPowerups = this.totalNumberOfFreeze;
+        this.isComplete = isComplete;
+        this.hitLocations = hitLocations;
+        //this.cubesCollected = cubesCollected;
+    }
+}
+
