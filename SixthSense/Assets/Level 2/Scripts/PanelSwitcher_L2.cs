254a21f7a15466e8b61a71236b3a667cf9962c08
Rishikesh Jaadhav <rishikeshjaadhav@gmail.com>
2023-03-06 19:09:31 -0800

    level 2 canvas fix

--- SixthSense/Assets/Level 2/Scripts/PanelSwitcher_L2.cs
+++ SixthSense/Assets/Level 2/Scripts/PanelSwitcher_L2.cs
@@ -7,6 +7,7 @@ public class PanelSwitcher_L2 : MonoBehaviour
     //public float switchTime = 0f;
     public CanvasGroup fromPanel;
     public CanvasGroup toPanel;
+    [SerializeField] GameObject EndGameCanvas;
     public float timer = 60.0f;
     public float popUpTime = 2.0f;
 
@@ -86,11 +87,12 @@ public class PanelSwitcher_L2 : MonoBehaviour
 
     public void switchpanel(){
         switchStarted = true;
-        fromPanel.alpha = 0f;
-        fromPanel.interactable = false;
-        fromPanel.blocksRaycasts = false;
-        toPanel.alpha = 1f;
-        toPanel.interactable = true;
-        toPanel.blocksRaycasts = true;
+        EndGameCanvas.SetActive(true);
+        // fromPanel.alpha = 0f;
+        // fromPanel.interactable = false;
+        // fromPanel.blocksRaycasts = false;
+        // toPanel.alpha = 1f;
+        // toPanel.interactable = true;
+        // toPanel.blocksRaycasts = true;
     }
 }
