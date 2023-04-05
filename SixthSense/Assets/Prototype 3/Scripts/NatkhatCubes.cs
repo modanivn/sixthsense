77b0ad54ab6ad6bf517996dbd603ae2ee6d0e14b
modanivn <103077198+modanivn@users.noreply.github.com>
2023-04-04 20:04:35 -0700

    Clean Code

--- SixthSense/Assets/Prototype 3/Scripts/NatkhatCubes.cs
+++ SixthSense/Assets/Prototype 3/Scripts/NatkhatCubes.cs
@@ -13,8 +13,6 @@ public class NatkhatCubes : MonoBehaviour
     public float powerUpTime = 10.0f;
 
     public void funWithCube(int index){
-        // System.Random random = new System.Random();
-        // int index = random.Next(1, functionArray.Length+1);
         
         switch(index){
             case 1:
@@ -54,7 +52,6 @@ public class NatkhatCubes : MonoBehaviour
 
     void UpdateCountdown(){
         powerUpTextElement.text = "Jump increased for " + Mathf.CeilToInt(powerUpTime).ToString() + " seconds.";
-        // Debug.Log(powerUpTime);
         powerUpTime -= 1.0f;
     }
 
@@ -64,7 +61,6 @@ public class NatkhatCubes : MonoBehaviour
         powerUpTextElement.text = "";
         powerUpTime = 10.0f;
         gameObject.GetComponent<Player_Movement>().decreaseForce(jumpForceMultiplier);
-        // powerUpTextElement.text = "Jump Decrease";
     }
 }
 
