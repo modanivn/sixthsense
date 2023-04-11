d04b56ec8009c27240e97b213078ca3ced817dc5
Rishikesh Jaadhav <rishikeshjaadhav@gmail.com>
2023-04-10 23:40:36 -0700

    gg commit

--- SixthSense/Assets/levelSelect.cs
+++ SixthSense/Assets/levelSelect.cs
@@ -6,20 +6,8 @@ using UnityEngine.SceneManagement;
 public class levelSelect : MonoBehaviour
 {
 
-    public void openLevel1() {
-        SceneManager.LoadScene(2);
-    }
-
-    public void openLevel2() {
-        SceneManager.LoadScene(3);
-    }
-
-    public void openLevel3() {
-        SceneManager.LoadScene(4);
-    }
-
-    public void openLevel4() {
-        SceneManager.LoadScene(5);
+    public void openLevel(int level) {
+        SceneManager.LoadScene(level);
     }
 
 }
